702ddd2cac037ed03cc723c60668076dfac82204
Ahmet Mert ÇALIŞ <calisahmetmert@gmail.com>
2021-02-11 22:07:28 +0300

    yazimhatasi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
             CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.Description + " " + car.DailyPrice + "--- TL ");
+                Console.WriteLine(car.Description + " ----" + car.DailyPrice +  "  TL");
             }
             
 
